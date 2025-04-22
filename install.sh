cf47f243d89102a588c1f5a7ac9f2afd714fc747
jflxb <jonas@blenninger.de>
2025-04-22 11:11:52 +0200

    quickfix to install script

--- install.sh
+++ install.sh
@@ -35,9 +35,8 @@ execute_command() {
 
 echo -e "${LINK}${NC} Installing the ${GREEN}custatevec${NC} and ${GREEN}lbfgs${NC} libraries${NC}"
 conda install custatevec conda-forge::liblbfgs
-# execute_command "pip install maturin[patchelf]" "Installing the ${GREEN}maturin${NC} build tool with ${GREEN}patchelf${NC}" "$PUZZLE"
-# execute_command "maturin build --release" "Building the project from source... (this might take a while)" "$GEAR "
-execute_command "uv build" "Building the project from source... (this might take a while)" "$GEAR "
+execute_command "make -C ./aqaoa/internal/" "[1/2] Building the project from source... (this might take a while)" "$GEAR "
+execute_command "uv build" "[2/2] Building the project from source... (this might take a while)" "$GEAR "
 
 WHEEL_FILE=$(find dist -name '*.whl' | sort | tail -n 1)
 
